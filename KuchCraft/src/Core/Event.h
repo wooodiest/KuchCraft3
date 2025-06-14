f506dfd867b5d188b5db0eb804eccfe91fbb2eaa
wooodiest <michalkuchnickiisc@gmail.com>
2025-06-14 22:32:14 +0200

    Added Application Events

---
+++ KuchCraft/src/Core/Event.h
@@ -0,0 +1,297 @@
+#pragma once
+
+#include <stdint.h>
+#include <utility>
+#include <string>
+
+#include "Core/Base.h"
+#include "Core/KeyCodes.h"
+
+namespace KuchCraft {
+
+	enum class EventType
+	{
+		None = 0,
+
+		/// Associated with window
+		WindowClose, WindowResize, WindowMoved,
+
+		/// Related to the use of the keyboard
+		KeyPressed, KeyReleased, KeyTyped,
+
+		/// Related to the use of the mouse
+		MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+	};
+
+	enum EventCategory
+	{
+		None = 0,
+		EventCategoryApplication = BIT(0),
+		EventCategoryInput       = BIT(1),
+		EventCategoryKeyboard    = BIT(2),
+		EventCategoryMouse       = BIT(3),
+		EventCategoryMouseButton = BIT(4)
+	};
+
+	class Event
+	{
+	public:
+		virtual ~Event() = default;
+
+		bool Handled = false;
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept = 0;
+
+		virtual [[nodiscard]] int GetCategoryFlags() const noexcept = 0;
+
+		[[nodiscard]] bool IsInCategory(EventCategory category) const noexcept
+		{
+			return GetCategoryFlags() & category;
+		}
+
+	};
+
+	/// Helper class to dispatch events based on their type. 
+	class EventDispatcher
+	{
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event) {
+		}
+
+		/// Dispatches the event to the appropriate handler function.
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.Handled |= func(static_cast<T&>(m_Event));
+				return true;
+			}
+			return false;
+		}
+
+	private:
+		Event& m_Event;
+
+	};
+
+	/// This event is triggered when the window is resized.
+	class WindowResizeEvent : public Event
+	{
+	public:
+		WindowResizeEvent(int width, int height)
+			: m_Width(width), m_Height(height) {
+		}
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::WindowResize; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept override { return GetStaticType(); }
+
+		virtual [[nodiscard]] int GetCategoryFlags() const noexcept override { return EventCategoryApplication; }
+
+		inline [[nodiscard]] int GetWidth() const noexcept { return m_Width; }
+
+		inline [[nodiscard]] int GetHeight() const noexcept { return m_Height; }
+
+	private:
+		int m_Width;
+		int m_Height;
+
+	};
+
+	/// This event is triggered when the window is closed.
+	class WindowCloseEvent : public Event
+	{
+	public:
+		WindowCloseEvent() = default;
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::WindowClose; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept override { return GetStaticType(); }
+
+		virtual [[nodiscard]] int GetCategoryFlags() const noexcept override { return EventCategoryApplication; }
+
+	};
+
+	/// This event is triggered when the window is moved.
+	class WindowMoveEvent : public Event
+	{
+	public:
+		WindowMoveEvent(int x, int y)
+			: m_X(x), m_Y(y) {
+		}
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::WindowMoved; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept override { return GetStaticType(); }
+
+		virtual [[nodiscard]] int GetCategoryFlags() const noexcept override { return EventCategoryApplication; }
+
+		inline [[nodiscard]] int GetX() const noexcept { return m_X; }
+
+		inline [[nodiscard]] int GetY() const noexcept { return m_Y; }
+
+	private:
+		int m_X;
+		int m_Y;
+	};
+
+	/// This class handles storing the key code and retrieving category flags.
+	class KeyEvent : public Event
+	{
+	public:
+		inline [[nodiscard]] KeyCode GetKeyCode() const noexcept { return m_KeyCode; }
+
+		virtual [[nodiscard]] int GetCategoryFlags() const noexcept override { return EventCategoryKeyboard | EventCategoryInput; }
+
+	protected:
+		KeyEvent(const KeyCode keycode)
+			: m_KeyCode(keycode) {
+		}
+
+		KeyCode m_KeyCode;
+
+	};
+
+	/// This event is triggered when a key is pressed and stores information
+	/// about whether the key press is a repeated action.
+	class KeyPressedEvent : public KeyEvent
+	{
+	public:
+		KeyPressedEvent(const KeyCode keycode, bool isRepeat = false)
+			: KeyEvent(keycode), m_IsRepeat(isRepeat) {
+		}
+
+		inline [[nodiscard]] bool IsRepeat() const noexcept { return m_IsRepeat; }
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::KeyPressed; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept override { return GetStaticType(); }
+
+	private:
+		bool m_IsRepeat;
+
+	};
+	/// This event is triggered when a key is released.
+	class KeyReleasedEvent : public KeyEvent
+	{
+	public:
+		KeyReleasedEvent(const KeyCode keycode)
+			: KeyEvent(keycode) {
+		}
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::KeyReleased; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept override { return GetStaticType(); }
+
+	};
+
+	/// This event is triggered when a character is typed (key press and release).
+	class KeyTypedEvent : public KeyEvent
+	{
+	public:
+		KeyTypedEvent(const KeyCode keycode)
+			: KeyEvent(keycode) {
+		}
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::KeyTyped; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept override { return GetStaticType(); }
+
+	};
+
+	/// This event is triggered when the mouse is moved.
+	class MouseMovedEvent : public Event
+	{
+	public:
+		MouseMovedEvent(float x, float y)
+			: m_MouseX(x), m_MouseY(y) {
+		}
+
+		inline [[nodiscard]] float GetX() const noexcept { return m_MouseX; }
+
+		inline [[nodiscard]] float GetY() const noexcept { return m_MouseY; }
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::MouseMoved; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept override { return GetStaticType(); }
+
+		virtual [[nodiscard]] int GetCategoryFlags() const noexcept override { return EventCategoryMouse | EventCategoryInput; }
+
+	private:
+		float m_MouseX;
+		float m_MouseY;
+
+	};
+
+	/// This event is triggered when the mouse wheel is scrolled.
+	class MouseScrolledEvent : public Event
+	{
+	public:
+		MouseScrolledEvent(const float xOffset, const float yOffset)
+			: m_XOffset(xOffset), m_YOffset(yOffset) {
+		}
+
+		inline [[nodiscard]] float GetXOffset() const noexcept { return m_XOffset; }
+
+		inline [[nodiscard]] float GetYOffset() const noexcept { return m_YOffset; }
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::MouseScrolled; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept override { return GetStaticType(); }
+
+		virtual [[nodiscard]] int GetCategoryFlags() const noexcept override { return EventCategoryMouse | EventCategoryInput; }
+
+	private:
+		float m_XOffset;
+		float m_YOffset;
+
+	};
+
+	/// This class stores the mouse button that triggered the event.
+	class MouseButtonEvent : public Event
+	{
+	public:
+		inline [[nodiscard]] MouseCode GetMouseButton() const noexcept { return m_Button; }
+
+		virtual [[nodiscard]] int GetCategoryFlags() const noexcept override { return EventCategoryMouse | EventCategoryInput | EventCategoryMouseButton; }
+
+	protected:
+		MouseButtonEvent(const MouseCode button)
+			: m_Button(button) {
+		}
+
+		MouseCode m_Button;
+
+	};
+
+	/// This event is triggered when a mouse button is pressed.
+	class MouseButtonPressedEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonPressedEvent(const MouseCode button)
+			: MouseButtonEvent(button) {
+		}
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::MouseButtonPressed; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept override { return GetStaticType(); }
+
+	};
+
+	/// This event is triggered when a mouse button is released.
+	class MouseButtonReleasedEvent : public MouseButtonEvent
+	{
+	public:
+		MouseButtonReleasedEvent(const MouseCode button)
+			: MouseButtonEvent(button) {
+		}
+
+		static inline [[nodiscard]] EventType GetStaticType() noexcept { return EventType::MouseButtonReleased; }
+
+		virtual [[nodiscard]] EventType GetEventType() const noexcept { return GetStaticType(); }
+
+	};
+
+}
---
+++ KuchCraft/src/Core/KeyCodes.h
@@ -0,0 +1,162 @@
+#pragma once
+
+#include <stdint.h>
+
+namespace KuchCraft {
+
+	typedef enum class KeyCode : uint16_t
+	{
+		Space      = 32, /// The space bar key.
+		Apostrophe = 39, /// Apostrophe key (').
+		Comma  = 44,     /// Comma key (,).
+		Minus  = 45,     /// Minus key (-).
+		Period = 46,     /// Period key (.).
+		Slash  = 47,     /// Slash key (/).
+
+		D0 = 48, /// number key: 0
+		D1 = 49, /// number key: 1
+		D2 = 50, /// number key: 2
+		D3 = 51, /// number key: 3
+		D4 = 52, /// number key: 4
+		D5 = 53, /// number key: 5
+		D6 = 54, /// number key: 6
+		D7 = 55, /// number key: 7
+		D8 = 56, /// number key: 8
+		D9 = 57, /// number key: 9
+
+		Semicolon = 59, /// Semicolon key (;).
+		Equal     = 61, /// Equal sign key (=).
+
+		/// Chars
+		A = 65,
+		B = 66,
+		C = 67,
+		D = 68,
+		E = 69,
+		F = 70,
+		G = 71,
+		H = 72,
+		I = 73,
+		J = 74,
+		K = 75,
+		L = 76,
+		M = 77,
+		N = 78,
+		O = 79,
+		P = 80,
+		Q = 81,
+		R = 82,
+		S = 83,
+		T = 84,
+		U = 85,
+		V = 86,
+		W = 87,
+		X = 88,
+		Y = 89,
+		Z = 90,
+
+		LeftBracket  = 91, /// key: [ 
+		Backslash    = 92, /// key: \ 
+		RightBracket = 93, /// key: ]
+		GraveAccent  = 96, /// key: `
+
+		World1 = 161, /// non-US #1
+		World2 = 162, /// non-US #2
+
+		/// Function keys
+		Escape      = 256,
+		Enter       = 257,
+		Tab         = 258,
+		Backspace   = 259,
+		Insert      = 260,
+		Delete      = 261,
+		Right       = 262,
+		Left        = 263,
+		Down        = 264,
+		Up          = 265,
+		PageUp      = 266,
+		PageDown    = 267,
+		Home        = 268,
+		End         = 269,
+		CapsLock    = 280,
+		ScrollLock  = 281,
+		NumLock     = 282,
+		PrintScreen = 283,
+		Pause       = 284,
+		F1  = 290,
+		F2  = 291,
+		F3  = 292,
+		F4  = 293,
+		F5  = 294,
+		F6  = 295,
+		F7  = 296,
+		F8  = 297,
+		F9  = 298,
+		F10 = 299,
+		F11 = 300,
+		F12 = 301,
+		F13 = 302,
+		F14 = 303,
+		F15 = 304,
+		F16 = 305,
+		F17 = 306,
+		F18 = 307,
+		F19 = 308,
+		F20 = 309,
+		F21 = 310,
+		F22 = 311,
+		F23 = 312,
+		F24 = 313,
+		F25 = 314,
+
+		/* Keypad */
+		KP0 = 320,
+		KP1 = 321,
+		KP2 = 322,
+		KP3 = 323,
+		KP4 = 324,
+		KP5 = 325,
+		KP6 = 326,
+		KP7 = 327,
+		KP8 = 328,
+		KP9 = 329,
+		KPDecimal  = 330,
+		KPDivide   = 331,
+		KPMultiply = 332,
+		KPSubtract = 333,
+		KPAdd      = 334,
+		KPEnter    = 335,
+		KPEqual    = 336,
+
+		LeftShift    = 340,
+		LeftControl  = 341,
+		LeftAlt      = 342,
+		LeftSuper    = 343,
+		RightShift   = 344,
+		RightControl = 345,
+		RightAlt     = 346,
+		RightSuper   = 347,
+		Menu         = 348
+
+	} Key;
+
+	typedef enum class MouseCode : uint16_t
+	{
+		Button0 = 0, /// Left mouse button.
+		Button1 = 1, /// Right mouse button.
+		Button2 = 2, /// Middle mouse button.
+
+		Button3 = 3,
+		Button4 = 4,
+		Button5 = 5,
+		Button6 = 6,
+		Button7 = 7,
+
+		ButtonLast   = Button7,
+		ButtonLeft   = Button0,
+		ButtonRight  = Button1,
+		ButtonMiddle = Button2 
+
+	} Mouse;
+
+}
