8b0cefd9cb6940da3630a63a0fb8aa478122f6f7
wooodiest <michalkuchnickiisc@gmail.com>
2025-06-30 20:18:39 +0200

    Added Uniform Buffers

---
+++ KuchCraft/src/Graphics/Core/UniformBuffer.cpp
@@ -0,0 +1,64 @@
+#include "kcpch.h"
+#include "Graphics/Core/UniformBuffer.h"
+
+#include <glad/glad.h>
+
+namespace KuchCraft {
+
+	UniformBuffer::UniformBuffer(size_t size)
+		: m_Size(size)
+	{
+		m_Binding = AllocateBinding();
+
+		glCreateBuffers(1, &m_RendererID);
+		glNamedBufferData(m_RendererID, m_Size, nullptr, GL_DYNAMIC_DRAW);
+		glBindBufferBase(GL_UNIFORM_BUFFER, m_Binding, m_RendererID);
+	}
+
+	UniformBuffer::~UniformBuffer()
+	{
+		if (IsValid())
+		{
+			glDeleteBuffers(1, &m_RendererID);
+			ReleaseBinding(m_Binding);
+		}
+	}
+
+	Ref<UniformBuffer> UniformBuffer::Create(size_t size)
+	{
+		return Ref<UniformBuffer>(new UniformBuffer(size));
+	}
+
+	void UniformBuffer::SetData(const void* data, uint32_t size, uint32_t offset)
+	{
+		glNamedBufferSubData(m_RendererID, offset, size, data);
+	}
+
+	void UniformBuffer::SetDebugName(const std::string& name)
+	{
+		KC_CORE_ASSERT(!name.empty(), "UniformBuffer debug name must not be empty.");
+		KC_CORE_ASSERT(IsValid(), "UniformBuffer is not valid.");
+
+		m_DebugName = name;
+		if (GLAD_GL_KHR_debug)
+			glObjectLabel(GL_BUFFER, m_RendererID, static_cast<GLsizei>(name.length()), name.c_str());
+	}
+
+	uint32_t UniformBuffer::AllocateBinding()
+	{
+		if (!s_FreeBindings.empty())
+		{
+			auto it = s_FreeBindings.begin();
+			uint32_t binding = *it;
+			s_FreeBindings.erase(it);
+			return binding;
+		}
+
+		return s_NextAvailableBinding++;
+	}
+
+	void UniformBuffer::ReleaseBinding(uint32_t binding)
+	{
+		s_FreeBindings.insert(binding);
+	}
+}
---
+++ KuchCraft/src/Graphics/Core/UniformBuffer.h
@@ -0,0 +1,42 @@
+#pragma once
+
+namespace KuchCraft {
+
+	class UniformBuffer
+	{
+	public:
+		~UniformBuffer();
+
+		static Ref<UniformBuffer> Create(size_t size);
+
+		void SetData(const void* data, uint32_t size, uint32_t offset = 0);
+
+		bool IsValid() const { return m_RendererID != 0; }
+
+		size_t GetSize() const { return m_Size; }
+		RendererID GetRendererID() const { return m_RendererID; }
+		uint32_t GetBinding() const { return m_Binding; }
+
+		void SetDebugName(const std::string& name);
+		const std::string& GetDebugName() const { return m_DebugName; }
+
+	private:
+		RendererID m_RendererID = 0;
+		size_t     m_Size    = 0;
+		uint32_t   m_Binding = 0;
+
+		std::string m_DebugName = "UnnamedUniformBuffer";
+
+		UniformBuffer(size_t size);
+
+		static inline std::set<uint32_t> s_FreeBindings;
+		static inline uint32_t s_NextAvailableBinding = 0;
+
+		static uint32_t AllocateBinding();
+		static void ReleaseBinding(uint32_t binding);
+
+		KC_DISALLOW_COPY(UniformBuffer);
+		KC_DISALLOW_MOVE(UniformBuffer);
+	};
+
+}
